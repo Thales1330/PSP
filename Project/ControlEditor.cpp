ddb262a4d6bed48ac68eeed99032e10c49c02996
Thales1330 <thaleslima.ufu@gmail.com>
2017-01-18 19:45:49 -0200

    Control editor implementation start

---
+++ Project/ControlEditor.cpp
@@ -0,0 +1,11 @@
+#include "ControlEditor.h"
+
+ControlEditor::ControlEditor(wxWindow* parent)
+    : ControlEditorBase(parent)
+{
+}
+
+ControlEditor::~ControlEditor()
+{
+}
+
---
+++ Project/ControlEditor.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLEDITOR_H
+#define CONTROLEDITOR_H
+#include "ControlEditorBase.h"
+
+class ControlEditor : public ControlEditorBase
+{
+public:
+    ControlEditor(wxWindow* parent);
+    virtual ~ControlEditor();
+};
+#endif // CONTROLEDITOR_H
